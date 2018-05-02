e1315e7938b28883b02bd393c19003fc382ebe44
Jake Evesque <Jake@JakesLaptop.EVESQUE.COM>
2018-05-02 12:28:18 -0500

    updated

---
+++ index.js
@@ -0,0 +1,30 @@
+
+let greeting = function() {
+    alert("Hello slacker!");
+}
+
+greeting();
+
+// greeting();
+// function greeting(){
+//     console.log("Hello, slacker!")
+// }
+
+
+// let fruits = ['apple', 'tomato', 'banana'];
+// //let favFruit;
+// function printFruits() {
+//     let favFruit = fruits[2];
+    
+//     console.log(fruits[0]);
+//     printFavFruit();
+//     function printFavFruit() {
+//         let leastFavFruit = fruits[0];
+//         console.log(favFruit);
+//     }
+//     console.log(leastFavFruit);    
+// }
+
+
+
+// printFruits();
